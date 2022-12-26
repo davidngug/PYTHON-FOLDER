2e86a14f3759efcf700aa1c3912d56dcfa8ecc3e
davidngug <davidkinyanjui245@gmail.com>
2022-12-26 12:09:30 +0300

    save

--- 4.3class.py
+++ 4.3class.py
@@ -3,7 +3,7 @@ count=0
 try:fname=open(user)
 except:print('WRONG FILE NAME')
 for subject in fname:
-        count=count+1
-        if subject.startswith('subject'):
+    count=count+1
+    if subject.startswith('subject'):
         print('the number of subjects is',count)
 quit()
