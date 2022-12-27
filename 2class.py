798938b669f987c1f59cd3ad2d2888220313bf4b
davidngug <davidkinyanjui245@gmail.com>
2022-12-27 12:02:23 +0300

    save

--- 2class.py
+++ 2class.py
@@ -21,9 +21,17 @@ print(b,6.0)
 b=b.capitalize()#Turns first letter to capital in variable
 print(b,7.0)
 print(b.lstrip(),8.0)#Removes space on left
-print(b.rstrip(),9.0)#removes space on right, refer 4.2Class.py 
+
+print(b.rstrip(),9.0)#removes space on right, refer 4.2Class.py
+ 
 print(b.strip(),10.0)#removes spaces generally infront nd back
+
 print(b.startswith(' fello'),11.0)#confirms variable starts with a specific str/int ( fello)
+
 print(b.endswith('there '),12.0)#confirms variable ends with a specific str/int (there )
 print(b.endswith('f'),13.0)#EXAMPLE
+
+print(b.split(),14.0)#changes a group of strings to a list using spaces
+s=('willow;jayson;phillips')#EGXAMPLE
+print(b.split(';'))#Can be used to seperate a bunch of strings to list using [;] but specifying can use any even if its a [/][,][.]
 quit()
