8d4001fc3da9ec854d3e7892d70f3c9f1685575b
davidngug <davidkinyanjui245@gmail.com>
2022-12-27 13:03:58 +0300

    save

--- 5class.py
+++ 5class.py
@@ -6,6 +6,7 @@ for items in filename:
         items=items.split()
         for item in items:
             if item.startswith('From ') or item.endswith('THU'):
-                if item.startswith('From ') or item.endswith('CWEN@IUPUI.EDU'):
-                    print(items,'firststage')
+                print(items,'first stage')
+                if item.startswith('From ') or item.endswith('2008'):
+                    print(items,'Second stage')
 quit()
