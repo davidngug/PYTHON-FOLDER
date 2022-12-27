49abf3242004a35172a4dc19577017ee367a7ed2
davidngug <davidkinyanjui245@gmail.com>
2022-12-27 12:25:50 +0300

    save

--- 5class.py
+++ 5class.py
@@ -1,6 +1,6 @@
 filename=open('Egxample2.txt')
 for items in filename:
-    items=items.rstrip()
+    items=items.strip()
     if items.startswith('From '):
         items=items.upper()
         items=items.split()
